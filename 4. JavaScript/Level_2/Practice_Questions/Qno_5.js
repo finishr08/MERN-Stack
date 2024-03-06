b6b9e92ac0f8ce1802d372622837451293d09c2a
Mustafa Ahmed <contactmustafa8@gmail.com>
2024-03-06 19:25:58 +0500

    Update

---
+++ 4. JavaScript/Level_2/Practice_Questions/Qno_5.js
@@ -0,0 +1,14 @@
+// Qs5. Write a program to find the largest of 3 numbers.
+
+
+let num1 = 12;
+let num2 = 24;
+let num3 = 18;
+
+if (num1 > num2 && num1>num3){
+    console.log(`NUM1 is Greater ${num1}`);
+}else if (num2 > num3){
+    console.log(`NUM2 is Greater ${num2}`);
+}else{
+    console.log(`NUM3 is Greater ${num3}`);
+}
