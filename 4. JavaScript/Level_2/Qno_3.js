76a2961ac59111822c484a948c5b483438560bec
Mustafa Ahmed <contactmustafa8@gmail.com>
2024-03-06 18:52:49 +0500

    Update

---
+++ 4. JavaScript/Level_2/Qno_3.js
@@ -0,0 +1,9 @@
+// a good string starts with 'a' and has length > 3
+
+let stri = "Ahmed";
+stri = stri.toLowerCase()
+if (stri[0] ==="a" && stri.length > 3){
+    console.log("This is a Good String!");
+}else{
+    console.log("Thsi is a Bad String!");
+}
