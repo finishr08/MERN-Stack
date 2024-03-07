aca8e882dd6efcf859b5e1e251f2104abd8c177c
Mustafa Ahmed <contactmustafa8@gmail.com>
2024-03-07 19:17:19 +0500

    Update

---
+++ 4. JavaScript/Level_3/PR.2/Pr.2.js
@@ -0,0 +1,9 @@
+//using splice 
+
+let start2 = ["january","july","march","august"];
+
+console.log(`Start Array: [${start2}]`);
+start2.splice(0,2,"july","june")
+
+console.log('----------');
+console.log(`Final Array: [${start2}]`);
