0d1eb07de6a5bda1256a8cbcd251b277e126e835
Mustafa Ahmed <contactmustafa8@gmail.com>
2024-03-07 19:14:57 +0500

    Update

--- 4. JavaScript/Level_3/PR.2/Pr.1.js
+++ 4. JavaScript/Level_3/PR.2/Pr.1.js
@@ -10,6 +10,8 @@ console.log('----------');
 console.log(`Final Array: [${start}]`);
 
 console.log("=================================");
+
+
 // Method : 2
 
 let start1 = ["january","july","march","august"];
@@ -20,3 +22,14 @@ start1.unshift("june")
 start1.unshift("july")
 console.log('----------');
 console.log(`Final Array: [${start1}]`);
+
+
+// Method:3
+
+let start2 = ["january","july","march","august"];
+
+console.log(`Start Array: [${start2}]`);
+start2.splice(0,2,"july","june")
+
+console.log('----------');
+console.log(`Final Array: [${start2}]`);
