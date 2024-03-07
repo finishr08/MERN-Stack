c37785973a2ede6da8c7f3ee67730cbd307cab5d
Mustafa Ahmed <contactmustafa8@gmail.com>
2024-03-07 20:04:12 +0500

    Update

--- 4. JavaScript/Level_3/h1.js
+++
@@ -1,4 +0,0 @@
-let str = "Hello";
-let firstChar = str.charAt(0); // "H"
-let charCode = str.charCodeAt(1); // 101 (ASCII code for 'e')
-let thirdChar = str[2]; // "l" (using square bracket notation)
