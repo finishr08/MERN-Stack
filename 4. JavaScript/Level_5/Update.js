a1a5377b958368e2669d10ce015cedc1df4c2227
Mustafa Ahmed <contactmustafa8@gmail.com>
2024-03-12 17:31:39 +0500

    Update

---
+++ 4. JavaScript/Level_5/Update.js
@@ -0,0 +1,11 @@
+const twitter = {
+    username : "@Mustafa",
+    content : `Mustafa is Software Engineer`,
+    likes: 240,
+    post : 5,
+    tags : ["#finishr","#mustafa"]
+}
+
+twitter.username = "finishr" // update username
+twitter.content = "Mustafa is a Mern Stack Developer" // overrite the exsisting
+console.log(twitter);
