244e01d7923b43c594834b72264db015abc0787e
Mustafa Ahmed <contactmustafa8@gmail.com>
2024-03-12 17:29:11 +0500

    Update

---
+++ 4. JavaScript/Level_5/Post.js
@@ -0,0 +1,9 @@
+const twitter = {
+    username : "@Mustafa",
+    content : `Mustafa is Software Engineer`,
+    likes: 240,
+    post : 5,
+    tags : ["#finishr","#mustafa"]
+}
+
+console.log(twitter);
