d96ea673fab0e8348422e7960de52abcf4551030
Mustafa Ahmed <contactmustafa8@gmail.com>
2024-03-13 17:18:27 +0500

    Update

---
+++ 4. JavaScript/Level_6/Q.1.js
@@ -0,0 +1,4 @@
+function poem() {
+    console.log("This is Poem!");
+}
+poem()
---
+++ 4. JavaScript/Level_6/Q.2.js
@@ -0,0 +1,6 @@
+function dice() {
+    let roll = Math.floor(Math.random() * 6) + 1 ;
+    console.log(roll);
+}
+dice()
+dice()
