eaa81264181e097c0d81d9dd7444d94034c36883
Mustafa Ahmed <contactmustafa8@gmail.com>
2024-03-14 10:13:01 +0500

    Update

---
+++ 4. JavaScript/Level_6/Method.js
@@ -0,0 +1,14 @@
+let object = {
+    add: function (a,b) {
+        return a + b
+    },
+    sub: function (a,b) {
+        return a - b
+    },
+    mul: function (a,b) {
+        return a * b
+    }
+}
+
+let output = object.add(2,3);
+console.log(output);
